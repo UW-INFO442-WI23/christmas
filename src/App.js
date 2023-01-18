e22d247489b84dbc2ca04cdc0a426c23be667f76
aaronho@uw.edu <aaronho@aaronh.local>
2023-01-18 11:55:59 -0800

    push basic webpage

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+        <img src={"https://i.etsystatic.com/12639277/r/il/955cb3/3421466341/il_1080xN.3421466341_77w0.jpg"}/>
+    </div>
+  );
+}
+
+export default App;
