ccd36bf08b151e4f5fc9be3082f4e09d5619edd8
aaronho@uw.edu <aaronho@aaronh.local>
2023-01-18 11:57:56 -0800

    add alt to img

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-        <img src={"https://i.etsystatic.com/12639277/r/il/955cb3/3421466341/il_1080xN.3421466341_77w0.jpg"}/>
+        <img src={"https://i.etsystatic.com/12639277/r/il/955cb3/3421466341/il_1080xN.3421466341_77w0.jpg"} alt="picture of tree"/>
     </div>
   );
 }
