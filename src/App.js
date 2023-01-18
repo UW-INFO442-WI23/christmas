09eba3942daf9d4f53a68addc94074d330ea2bc7
Takumi Shimada <tsjuly171996@gmail.com>
2023-01-18 13:50:12 -0800

    test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="App">
         <img src={"https://i.etsystatic.com/12639277/r/il/955cb3/3421466341/il_1080xN.3421466341_77w0.jpg"} alt="picture of tree"/>
+        <p>just checking yo</p>
     </div>
   );
 }
