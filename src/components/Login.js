94d165e807104789a7b1ba05515c1b6bbc40e771
Brian Ho <brianho34@yahoo.com>
2023-02-01 15:18:09 -0800

    set up login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,29 @@
-
 import React from 'react';
+import { redirect } from 'react-router-dom';
+import StyledFireBaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import { getAuth, EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
 
-function Login() {
-    return (
-        <h1>Login Page</h1>
-    );
+
+const firebaseUIConfig = {
+    signInOptions: [
+        {provider: EmailAuthProvider.PROVIDER_ID, requiredDisplayName: true},
+        GoogleAuthProvider.PROVIDER_ID,
+    ],
+    signInFlow: 'popup',
+    credentialHelper: 'none',
+    callbacks: {
+        signInSuccessWithAuthResult: () => {
+            return false;
+        }
+    }
 }
-
+function Login(props) {
+    const auth = getAuth();
+    if(props.loggedIn) {
+        return redirect("/");
+    } else {
+        return <StyledFireBaseAuth uiConfig={firebaseUIConfig} firebaseAuth={auth}/>;
+    }
+    
+}
 export default Login;
