e7c8e62b1cbfe3a076081de1c800f656bf1ca8dc
hemmys <clauked@uw.edu>
2023-02-22 19:59:42 -0800

    added images to public folder, fixed card containers

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,14 +3,14 @@ import React from 'react';
 export function Card(props) {
     return (
         <div className="card m-1">
-            <div class="card-body">
+            <div id="about-card" class="card-body">
                 <div class="row">
                     <div class="col-sm-auto col-xl-12">
-                        <img src={`../img/${props.name}.png`} alt={props.name}></img>
+                        <img className="img-thumbnail img-fluid" src={`../img/${props.name}.png`} alt={props.name}></img>
                     </div>
                     <div className="col-sm col-xl-12">
                         <h2 className="card-title">{props.name}</h2>
-                        <p className="col-xs-5 col-md-12 m-4 p-6">{props.desc}</p>
+                        <p className="card-text m-4 p-6">{props.desc}</p>
                     </div>
                 </div>
             </div>
